7e3b5be0a7c35ee56291960be9e307242bb93d90
Pavlo Hilei <p.hilei@ucu.edu.ua>
2020-11-27 17:12:16 +0200

    Fix coverage of Student and SmartArrayApp

--- src/test/java/ua/edu/ucu/SmartArrayAppTest.java
+++ src/test/java/ua/edu/ucu/SmartArrayAppTest.java
@@ -1,7 +1,10 @@
 package ua.edu.ucu;
 
 import org.junit.Test;
-import ua.edu.ucu.smartarr.BaseArray;
+import ua.edu.ucu.functions.MyComparator;
+import ua.edu.ucu.functions.MyFunction;
+import ua.edu.ucu.functions.MyPredicate;
+import ua.edu.ucu.smartarr.*;
 
 import static org.junit.Assert.*;
 
@@ -38,5 +41,61 @@ public class SmartArrayAppTest {
         String[] expectedStudentNames = {"Borek Tomasz", "Kranga Antons", "Sutter Burr"};
 
         assertArrayEquals(expectedStudentNames, studentNames);
+    }
+
+    @Test
+    public void operationDescriptionTest() {
+        SmartArray arr = new BaseArray(new Object[]{1, 2, 3, 4, 5, 5, -9, 12, -14});
+        assertEquals(arr.operationDescription(), "ua.edu.ucu.smartarr.BaseArray");
+
+        arr = new DistinctDecorator(arr);
+        assertEquals(arr.operationDescription(), "ua.edu.ucu.smartarr.DistinctDecorator");
+
+        arr = new FilterDecorator(arr, new MyPredicate() {
+            @Override
+            public boolean test(Object t) {
+                return ((Integer) t) > 0;
+            }
+        });
+        assertEquals(arr.operationDescription(), "ua.edu.ucu.smartarr.FilterDecorator");
+
+        arr = new MapDecorator(arr, new MyFunction() {
+            @Override
+            public Object apply(Object t) {
+                return ((Integer)t) * ((Integer)t);
+            }
+        });
+        assertEquals(arr.operationDescription(), "ua.edu.ucu.smartarr.MapDecorator");
+
+        arr = new SortDecorator(arr, new MyComparator() {
+            @Override
+            public int compare(Object firstObj, Object secondObj) {
+                return (((Integer)firstObj) > ((Integer)secondObj)) ? 1 : 0;
+            }
+        });
+    }
+
+    @Test
+    public void studentEqualsTest() {
+        Student[] students = {
+                new Student("Ivar", "Grimstad", 3.9, 2),
+                new Student("Ittai", "Zeidman", 4.5, 1),
+                new Student("Ittai", "Zeidman", 4.5, 1)};
+        assertEquals(students[1], students[2]);
+        assertNotEquals(students[0], students[1]);
+        assertEquals(students[0], students[0]);
+        assertNotEquals(students[0], null);
+        assertNotEquals(students[0], new Object());
+    }
+
+    @Test
+    public void studentTestToString() {
+        Student student = new Student("Pavlo", "Hilei", 12000, 2);
+        assertEquals(student.toString(), "Student{name=Pavlo, surname=Hilei, GPA=12000.0, year=2}");
+    }
+
+    @Test
+    public void smartArrayAppConstructor() {
+        SmartArrayApp app = new SmartArrayApp();
     }
 }
